e788179de662efc9d6ecfd26acdcc8f0a91bdd4b
lzping <1284561302@qq.com>
2020-05-09 18:25:05 +0800

    a baseline that can run

--- train_baseline.py
+++ train_baseline.py
@@ -1,10 +1,14 @@
+import os
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+
+from read_dataset import ReadDataset
 
 # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class BPbaseline(nn.Module):
-    def __init__(self,input_dim = 7):
+    def __init__(self,input_dim = 8):
         super(BPbaseline, self).__init__()
         self.common_layer = nn.Sequential(
             nn.Linear(input_dim, 4096),
@@ -43,8 +47,37 @@ class BPbaseline(nn.Module):
             return loss
 
 if __name__ == '__main__':
-    network = BPbaseline()
-    rand_input = (torch.rand(1,7),torch.rand(1,10),torch.rand(1,2))
-    adam = torch.optim.Adam(network.parameters(), lr=0.00001)
-    loss = network(rand_input[0],rand_input[1],rand_input[2])
-    print('loss:{:.4f}'.format(loss))
+    batch_size = 512
+    learning_rate = 0.00001
+    epoch_num = 100
+    # 导入数据集
+    train_data = ReadDataset('train_preliminary/user_ad_info.csv')
+    train_loader = DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True)
+
+    # 构造网络
+    network = BPbaseline(input_dim=6)
+    if os.path.exists("baseline.pkl"):
+        network.load_state_dict(torch.load("baseline.pkl"))
+        print("[+] Load model Sucessfully")
+
+    # 设置优化器
+    adam = torch.optim.Adam(network.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.StepLR(adam, step_size=1, gamma=0.98)
+
+    print('train begin .................................')
+    network.train()
+    for epoch in range(epoch_num):
+        scheduler.step()
+        epoch_loss = 0
+        for batch,data in enumerate(train_loader):
+
+            loss = network(data[0].float(),data[1].float(),data[2].float())
+            adam.zero_grad()
+            loss.backward()
+            adam.step()
+            epoch_loss += loss
+            print("epoch:{},batch:{},batchloss:{:.4f},".format(epoch,batch,loss))
+
+        torch.save(network.state_dict(),"baseline.pkl")
+        print("epoch:{},epoch loss:{:.4f}".format(epoch,epoch_loss))
+    print("training finished")
